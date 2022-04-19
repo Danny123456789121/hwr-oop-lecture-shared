755bbbb0f89f5f0c976f70d4e24d181e9015a686
Daniel Kurth <daniel.kurth@sap.com>
2022-04-19 15:35:45 +0200

    Add base game Tic Tac Toe

--- src/main/java/hwr/oop/exercises/tictactoe/TicTacToe.java
+++ src/main/java/hwr/oop/exercises/tictactoe/TicTacToe.java
@@ -2,26 +2,73 @@ package hwr.oop.exercises.tictactoe;
 
 class TicTacToe {
 
-    TicTacToe() {
-        // TODO implement!
-    }
+    int[][] squares;
+    boolean crossSet;
+    boolean circleSet;
 
+    TicTacToe() {
+        squares = new int[3][3];
+    }
     int getValueAt(int x, int y) {
-        // TODO implement!
-        return -1;
+        return squares[x][y];
     }
 
     void setCross(int x, int y) {
-        // TODO implement!
+            if (isGameOver()) {
+                throw new RuntimeException("Game is over!");
+            } else if (squares[x][y] == 0 && getValueAt(x,y) == 0 && !crossSet) {
+                squares[x][y] = 1;
+                crossSet = true;
+                circleSet = false;
+            } else throw new RuntimeException("tried to check same square twice");
+
     }
 
     void setCircle(int x, int y) {
-        // TODO implement!
+            if (isGameOver()) {
+                throw new RuntimeException("Game is over!");
+            } else if (squares[x][y] == 0 && getValueAt(x,y) == 0 && !circleSet) {
+                squares[x][y] = 2;
+                circleSet = true;
+                crossSet = false;
+            } else throw new RuntimeException("tried to check same square twice");
     }
 
     boolean isGameOver() {
-        // TODO implement!
-        return false;
+        return tie() || crossWon() || circleWon();
     }
 
+    boolean crossWon(){
+        for (int x = 0; x < 3; x++) {
+            if (squares[x][0] == 1 && squares[x][1] == 1 && squares[x][2] == 1)
+                return true;
+        }
+        for (int y = 0; y < 3; y++) {
+            if (squares[0][y] == 1 && squares[1][y] == 1 && squares[2][y] == 1)
+                return true;
+        }
+        return squares[0][0] == 1 && squares[1][1] == 1 && squares[2][2] == 1 || squares[0][2] == 1 && squares[1][1] == 1 && squares[2][0] == 1;
+    }
+
+    boolean circleWon(){
+        for (int x = 0; x < 3; x++) {
+            if (squares[x][0] == 2 && squares[x][1] == 2 && squares[x][2] == 2)
+                return true;
+        }
+        for (int y = 0; y < 3; y++) {
+            if (squares[0][y] == 2 && squares[1][y] == 2 && squares[2][y] == 2)
+                return true;
+        }
+        return squares[0][0] == 2 && squares[1][1] == 2 && squares[2][2] == 2 || squares[0][2] == 2 && squares[1][1] == 2 && squares[2][0] == 2;
+    }
+
+    boolean tie(){
+        for (int[] u: squares) {
+            for (int elem: u) {
+                if(elem == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
 }
